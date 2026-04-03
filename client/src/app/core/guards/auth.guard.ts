fffc63aeceed69c34a6414a0ac60bfeeafeebaf4
sru9955 <dummy@gmail.com>
2026-04-03 17:44:52 +0000

    changes

--- client/src/app/core/guards/auth.guard.ts
+++ client/src/app/core/guards/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+//ram
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
